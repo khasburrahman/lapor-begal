9be887421d68e67013d43183033e2b3228fb9f86
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-05-23 15:33:46 +0700

    use only url for production

--- config/config.js
+++ config/config.js
@@ -9,10 +9,7 @@ module.exports = {
     "dialect": "postgres"
   }, 
   "production": {
-    "username": process.env.DATABASE_USERNAME,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE_NAME,
-    "host": process.env.DATABASE_URL,
+    "url": process.env.DATABASE_URL,
     "dialect": "postgres"
   }
 }
