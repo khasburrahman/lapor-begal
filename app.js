835ba32cc11a18415a3b6a054f8c9f1543c83593
aryadimade <madearyadi@gmail.com>
2019-05-23 14:32:41 +0700

    add home page skeleton

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs")
 app.use('/users', userRouter);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.render('home');
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,4 +5,5 @@ const UserController = require('../controllers/userController');
 router.get('/register', UserController.displayForm);
 router.post('/register', UserController.register);
 
+
 module.exports = router;
