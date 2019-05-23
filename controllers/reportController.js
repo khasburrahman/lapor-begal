4a0829d2f330ee285a4f95cd9e55f8a155600ae7
aryadimade <madearyadi@gmail.com>
2019-05-23 16:36:14 +0700

    list laporan skeleton done

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -4,6 +4,10 @@ class ReportController {
   static addReport(req, res) {
     res.render('report/addReport');
   }
+
+  static showList(req, res) {
+    res.render('report/home');
+  }
 }
 
 module.exports = ReportController;
--- routes/reportRouter.js
+++ routes/reportRouter.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const ReportController = require('../controllers/reportController');
 
+router.get('/', ReportController.showList);
 router.get('/addReport', ReportController.addReport);
 
 module.exports = router;
