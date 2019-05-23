59dcf5349cc8c8e0b3ed7b20721cbd4dde94b3b3
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-05-23 08:59:46 +0700

    add GET login route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,5 +2,6 @@ var express = require('express');
 var router = express.Router();
 const UserController = require('../controllers/userController');
 
-router.get('/register', UserController.register);
+router.get('/register', UserController.register)
+router.get('/login', UserController.getLogin)
 module.exports = router;
