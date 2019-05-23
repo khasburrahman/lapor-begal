f42eeee9f0fcf83c1d51746a6ae2d388e170519c
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-05-23 15:17:43 +0700

    use env port when in heroku

--- app.js
+++ app.js
@@ -18,4 +18,4 @@ app.use(flash({ locals: 'msg' }))
 app.use('/users', userRouter);
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || 5000, () => console.log(`Example app listening on port ${port}!`))
