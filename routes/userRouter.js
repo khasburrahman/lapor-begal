f4c0bf2cf4e437fc471c10ddb21dfca7962d528a
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-05-24 06:05:02 +0700

    logout

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const UserController = require('../controllers/userController');
+const uploadMiddleware = require('../middlewares/uploadMulterCloudinary')
 
 router.get('/register', UserController.displayForm)
-router.post('/register', UserController.register);
+router.post('/register', uploadMiddleware.single('foto'), UserController.register);
 router.get('/login', UserController.getLogin)
 router.post('/login', UserController.postLogin)
+router.get('/logout', UserController.logout)
+
 module.exports = router;
